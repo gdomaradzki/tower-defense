5c10d6c1d4351863f97925e54b896e05d95d6ca3
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-30 13:33:09 -0200

    Add map, enemies, game manager, pathfinding, enemy spawn and assets

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int target = 0;
+    public Transform exitPoint;
+    public Transform[] checkPoints;
+    public float navigationUpdate;
+
+    private Transform enemy;
+    private float navigationTime = 0;
+
+	// Use this for initialization
+	void Start () {
+        enemy = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (checkPoints != null) {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate) {
+                if (target < checkPoints.Length) {
+                    enemy.position = Vector2.MoveTowards(enemy.position, checkPoints[target].position, navigationTime);
+                } else {
+                    enemy.position = Vector2.MoveTowards(enemy.position, exitPoint.position, navigationTime);
+                }
+
+                navigationTime = 0;
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Checkpoint") {
+            target++;
+        } else if (collision.tag == "Finish") {
+            GameManager.instance.RemoveEnemyFromScreen();
+            Destroy(gameObject);
+        }
+    }
+}
